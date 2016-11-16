c827a842cd29818050fccc62d141379ec1609040
vidhyal <vvl.1990@gmail.com>
2016-11-15 21:43:57 -0800

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     flightnumber = parameters.get("flightnumber")
 
-    x = (getSchedule("flightnumber", flightnumber))
+    x = (getSchedule("flightnumber", FlightNumber))
     
     if (x):
       x = x[0]
